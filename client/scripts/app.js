fab297afeaf4b90f4a6bf736d4680b358dd73224
jdlawrence <jamil.lawrence@gmail.com>
2016-02-12 17:13:38 -0800

    Fixed some pathing issues

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,16 +1,43 @@
+var Jamil = React.createClass({
+  render: function() {
+    return (
+      <div >
+        My component
+      </div>
+      )
+  }
+});
 
 
 var App = React.createClass({
   render: function() {
     return (
-      <div>
+      <div className="something">
+        <Jamil />      
         Hello, world!
       </div>
       )
   }
 })
 
+var Counter = React.createClass({
+  getInitialState: function() {
+
+    return {count: this.props.initialCount};
+  },
+
+  handleClicker: function() {
+    this.setState({count: this.state.count + 1});
+    console.log('this.state', this.state);
+  },
+
+  render: function() {
+    return <div onClick={this.handleClicker}>{this.state.count}</div>;
+  }
+});
+
+
 React.render(
-  <App />,
+  <Counter initialCount={7} />,
   document.getElementById('app')
-  );
+);
